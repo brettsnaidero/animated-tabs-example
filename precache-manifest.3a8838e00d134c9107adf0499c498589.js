e33c2558c64468eab06beaa5941a487992ef0355
Brett Snaidero <brettsnaidero@hotmail.com>
2019-11-06 22:47:30 +1100

    Updates

---
+++ precache-manifest.3a8838e00d134c9107adf0499c498589.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a56cb2b33562cc037f51f3a973b092c",
+    "url": "/animated-tabs-example/index.html"
+  },
+  {
+    "revision": "8571fcaa3d8c84a84737",
+    "url": "/animated-tabs-example/static/css/main.1767625e.chunk.css"
+  },
+  {
+    "revision": "0d51107bbb02fe8887b0",
+    "url": "/animated-tabs-example/static/js/2.8b2428e6.chunk.js"
+  },
+  {
+    "revision": "8571fcaa3d8c84a84737",
+    "url": "/animated-tabs-example/static/js/main.1348fcdc.chunk.js"
+  },
+  {
+    "revision": "027f4a7215ce9d685574",
+    "url": "/animated-tabs-example/static/js/runtime-main.17b19cfe.js"
+  }
+]);
